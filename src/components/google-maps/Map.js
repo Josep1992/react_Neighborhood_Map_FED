05cf08358c0a61529736ba74b52bdffa568fc718
Josep López <jayden.haylie@gmail.com>
2018-10-28 00:58:34 -0400

    Map renders to the dom

--- src/components/google-maps/Map.js
+++ src/components/google-maps/Map.js
@@ -1,38 +1,56 @@
 import React, { Component } from 'react';
 
 class Map extends Component {
-  componentWillMount = () => {
-    this.initializeWindow();
-  };
   componentDidMount = () => {
-    this.initMap();
+    this.initializeWindow()
+      .then((google) => console.log(`${google} in window`))
+      .catch((error) => console.log({ error }));
   };
 
   injectGoogleMapsScript = () => {
-    // key fro the google maps api
-    const key = 'AIzaSyCG7x3FPh3k9bXDAPNyU_RxZtNpslixoLM';
+    // key from the google maps api
+    const key = 'AIzaSyCNYw-grWNRMrfIxpHpGzCFpd7mLX4k4Oo';
     // script src for injecting once the dom has loaded
     const source = `https://maps.googleapis.com/maps/api/js?key=${key}&callback=initMap`;
+    const body = document.body;
 
-    let script = document.querySelector('script');
-    script.src = source;
-    script.async = true;
-    script.defer = true;
+    let googleMapScript = document.createElement('script');
+    googleMapScript.src = source;
+    // The script must be async, it will be added to the dom once every elements is on the page.**
+    googleMapScript.async = true;
+    googleMapScript.defer = true;
+
+    // Adding the script before the ending body tag
+    body.insertAdjacentElement('beforeend', googleMapScript);
   };
 
   initializeWindow = () => {
-    this.injectGoogleMapsScript();
-    window.initMap = this.initMap;
+    return new Promise((resolve, reject) => {
+      if (!window.google) {
+        this.injectGoogleMapsScript();
+        window.initMap = this.initMap;
+        resolve('Google Object in Window');
+      } else {
+        reject('Google Object not in Window');
+      }
+    });
   };
 
   initMap = () => {
-    const map = new window.google.maps.Map(document.getElementById('map'), {
-      center: { lat: -34.397, lng: 150.644 },
-      zoom: 8,
+    let map;
+    map = new window.google.maps.Map(document.getElementById('map'), {
+      center: { lat: -66.5878767, lng: 18.4058278 },
+      zoom: 9,
     });
+
+    return map;
   };
   render() {
-    return <div role="application" id="map" />;
+    return (
+      <main>
+        <div role="application" id="map" />
+      </main>
+    );
   }
 }
 
