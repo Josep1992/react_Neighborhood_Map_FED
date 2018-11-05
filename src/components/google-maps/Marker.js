46608fd731ce820748579361aac9b70e1c5f2fc7
Josep López <jayden.haylie@gmail.com>
2018-11-04 23:00:32 -0400

    deleted

--- src/components/google-maps/Marker.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Marker = () => {
-  return <div>{/*  */}</div>;
-};
-
-export default Marker;
--- src/utilities/yelpKeys.js
+++
@@ -1,3 +0,0 @@
-export const yelp = {
-  clientId: 'g2DO5cHqyA-H1_PXsts3Sg',
-};
