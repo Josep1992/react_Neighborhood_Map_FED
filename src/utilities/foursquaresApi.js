63f07bb1e11988a7c88a6aed058c65e9b29943e0
Josep López <jayden.haylie@gmail.com>
2018-11-12 14:55:19 -0400

    Added more restaurants

--- src/utilities/foursquaresApi.js
+++ src/utilities/foursquaresApi.js
@@ -2,7 +2,7 @@ const fourSquares = {
   client_id: 'XNSAG0PISJ411W3EJODIW51APQE2EXZ2RASS5DLQZDIAMKLV',
   secret: 'Y4PC0YTCUZ0EMHUVPDPEYZBRYIYKGFAZTCF4EWHV2E3M3WHR',
   query: 'restaurants',
-  limit: 10,
+  limit: 30,
   near: 'San Juan',
 };
 
